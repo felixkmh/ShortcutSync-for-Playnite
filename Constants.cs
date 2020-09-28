a688beb64c232b0e002fd48458574d3d0a0e4a1a
Felix Holler <24227002+felixkmh@users.noreply.github.com>
2020-09-28 05:41:26 +0200

    added path constants

--- Constants.cs
+++ Constants.cs
@@ -1,5 +1,7 @@
 namespace ShortcutSync {
     static class Constants {
         public const string UNDEFINEDSOURCE = "Undefined";
+        public const string LAUNCHSCRIPTFOLDERNAME = ".launchScripts";
+        public const string ICONFOLDERNAME = "icons";
     }
 }
