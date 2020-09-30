5691dd50e2ed0a2743f85809864fdacaa0051a21
Felix Holler <24227002+felixkmh@users.noreply.github.com>
2020-09-30 14:37:31 +0200

    created shortcut interface

---
+++ Shortcut.cs
@@ -0,0 +1,35 @@
+﻿using Octokit;
+using Playnite.SDK.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ShortcutSync
+{
+    public abstract class Shortcut<T>
+    {
+        internal Shortcut() {}
+
+        public T TargetObject { get; protected set; }
+        public string ShortcutPath { get; protected set;  }
+
+        public abstract DateTime LastUpdated { get; protected set; }
+        public abstract bool Exists { get; }
+        public abstract string Name { get; set; }
+        public abstract bool IsValid { get; }
+        public abstract bool IsOutdated { get; }
+
+        public abstract bool Move(string path);
+        public abstract bool Create();
+        public abstract bool Remove();
+        public abstract bool Update(bool forceUpdate = false);
+        public abstract bool Update(T targetObject);
+        public void CreateOrUpdate()
+        {
+            if (Exists)
+                Update();
+            else if (IsValid)
+                Create();
+        }
+    }
+}
