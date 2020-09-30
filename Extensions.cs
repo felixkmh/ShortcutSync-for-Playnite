608f7eb3e3d419ee6a49ac8c3d1e1935a919a492
Felix Holler <24227002+felixkmh@users.noreply.github.com>
2020-09-30 14:37:12 +0200

    added some convenience methods

---
+++ Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortcutSync
+{
+    public static class Extensions
+    {
+        public static string GetSafeFileName(this string validName)
+        {
+            foreach (var c in Path.GetInvalidFileNameChars()) validName = validName.Replace(c.ToString(), "");
+            return validName;
+        }
+
+        public static string ToHexCode(this Color color)
+        {
+            return $"#{color.R:X2}{color.G:X2}{color.B:X2}";
+        }
+
+        public static bool AddMissing<Key, Value>(this IDictionary<Key, Value> dict, Key key, Value value)
+        {
+            if (dict.ContainsKey(key))
+            {
+                return false;
+            } else
+            {
+                dict.Add(key, value);
+                return true;
+            }
+        }
+    }
+}
