fd0ddb63a8ea1bf415bdc6201f116493702c0992
Felix Holler <24227002+felixkmh@users.noreply.github.com>
2020-10-02 13:15:58 +0200

    added method to check for valid path strings

--- Extensions.cs
+++ Extensions.cs
@@ -16,9 +16,10 @@ namespace ShortcutSync
             return validName;
         }
 
-        public static string ToHexCode(this Color color)
+        public static string ToHexCode(this Color color, bool includeAlpha = false)
         {
-            return $"#{color.R:X2}{color.G:X2}{color.B:X2}";
+            return $"#{color.R:X2}{color.G:X2}{color.B:X2}" 
+                + (includeAlpha?color.A.ToString("X2"):string.Empty);
         }
 
         public static bool AddMissing<Key, Value>(this IDictionary<Key, Value> dict, Key key, Value value)
@@ -31,6 +32,20 @@ namespace ShortcutSync
                 dict.Add(key, value);
                 return true;
             }
+        }
+
+        public static bool IsValidPath(this string path)
+        {
+            var invalidChars = Path.GetInvalidPathChars();
+            return !path.ToCharArray().Any(c => invalidChars.Contains(c));
+        }
+
+        public enum PathType { Invalid, File, Directory }
+        public static PathType GetPathType(this string path)
+        {
+            if (!path.IsValidPath()) return PathType.Invalid;
+            if (Path.HasExtension(path) && Path.GetFileNameWithoutExtension(path) != string.Empty) return PathType.File;
+            return PathType.Directory;
         }
     }
 }
